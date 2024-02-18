c324276817c56d3cb13fea5c4f3dcb60335b124e
murilonp <murilloprado457@gmail.com>
2024-02-18 12:57:51 -0300

    chore: add pg and dotenv setup

---
+++ src/db/postgres/helper.js
@@ -0,0 +1,22 @@
+import pg from 'pg';
+
+const { Pool } = pg;
+
+export const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+});
+
+export const PostgresHelper = {
+  query: async (query, param) => {
+    const client = await pool.connect();
+    const results = await client.query(query, param);
+
+    await client.release();
+
+    return results.rows;
+  },
+};
