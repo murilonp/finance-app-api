209094dcea331109aea28f4b63e8bdf874e6f3f8
murilonp <murilloprado457@gmail.com>
2024-02-23 19:27:33 -0300

    feat: ensure 404 is returned if no user is found in GetUserByIdController

--- src/controllers/get-user-by-id.js
+++ src/controllers/get-user-by-id.js
@@ -1,4 +1,4 @@
-import { badRequest, ok, serverError } from './helpers.js';
+import { badRequest, notFound, ok, serverError } from './helpers.js';
 import { GetUserByIdUseCase } from '../use-cases/get-user-by-id.js';
 import validator from 'validator';
 
@@ -13,6 +13,10 @@ export class GetUserByIdController {
 
       const getUserByIdUseCase = new GetUserByIdUseCase();
       const user = await getUserByIdUseCase.execute(httpRequest.params.userId);
+
+      if (!user) {
+        return notFound({ message: 'User not found' });
+      }
 
       return ok(user);
     } catch (error) {
